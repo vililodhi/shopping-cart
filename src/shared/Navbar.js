d6e840eec5ac89b24bd7280033235f38c896bc69
vaishali lodhi <vaishali.lodhi@sts.in>
2018-12-19 15:11:25 +0530

    modification

---
+++ src/shared/Navbar.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+import SearchBar from '../components/SearchBarComponent';
+
+class Menubar extends Component {
+    render() {
+        return (
+            <Navbar className="fixed-top" dark color="primary" expand="md">
+                <NavbarBrand href="/">Shopping Cart</NavbarBrand>
+                <SearchBar />
+                <Nav className="ml-auto" navbar>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Hello,
+                            <b> Your Orders</b>
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem href="/">
+                                Login
+                            </DropdownItem>
+                            <DropdownItem>
+                                New Customer? Starts Here
+                            </DropdownItem>
+                            <DropdownItem href="/">
+                                Additional Features
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Try
+                            <b> Prime</b>
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem href="/">
+                                Join Prime
+                            </DropdownItem>
+                            <DropdownItem href="/">
+                                Manage Prime
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                    <NavItem>
+                        <NavLink href="/"><i className="shopping cart icon"></i>Cart</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
+export default Menubar;
