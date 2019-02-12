024d3ce17b4da309a7f907cc6777a3f4da1e817f
vaishali lodhi <vaishali.lodhi@sts.in>
2019-02-12 13:06:32 +0530

    re-route

--- src/facebook/CommentDetails.js
+++ src/facebook/CommentDetails.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
+import CurrentPosition from '../position/CurrentPosition';
+import Navbar from '../shared/Navbar';
 
 const CommentDetail = (props) => {
     return (
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={props.avatar} />
-            </a>
-            <div className="content">
-                <a href="/" className="author">{props.author}</a>
+        <div className="row top-margin">
+            <Navbar />
+            <div className="col-md-6">
+                <div className="ui container comments">
+                    <ApprovalCard>
+                        <a href="/" className="avatar">
+                            <img alt="avatar" src={faker.image.avatar()} />
+                        </a>
+                        <div className="content">
+                            <a href="/" className="author">{faker.name.firstName()}</a>
+                        </div>
+                        <div className="metadata">
+                            <span className="date">Today at 2:27PM</span>
+                        </div>
+                        <div className="text">This is fake data!</div>
+                    </ApprovalCard>
+                    <ApprovalCard>
+                        <a href="/" className="avatar">
+                            <img alt="avatar" src={faker.image.avatar()} />
+                        </a>
+                        <div className="content">
+                            <a href="/" className="author">{faker.name.firstName()}</a>
+                        </div>
+                        <div className="metadata">
+                            <span className="date">Today at 3:37PM</span>
+                        </div>
+                        <div className="text">This is fake data!</div>
+                    </ApprovalCard>
+                    <ApprovalCard>
+                        <a href="/" className="avatar">
+                            <img alt="avatar" src={faker.image.avatar()} />
+                        </a>
+                        <div className="content">
+                            <a href="/" className="author">{faker.name.firstName()}</a>
+                        </div>
+                        <div className="metadata">
+                            <span className="date">Today at 4:47PM</span>
+                        </div>
+                        <div className="text">This is fake data!</div>
+                    </ApprovalCard>
+                </div>
             </div>
-            <div className="metadata">
-                <span className="date">{props.timeAgo}</span>
+            <div className="col-md-6">
+                <b><CurrentPosition /></b>
             </div>
-            <div className="text">{props.content}</div>
         </div>
     );
 }
