f7d95e14612b26974afc9f36baffed479d69f64b
vaishali lodhi <vaishali.lodhi@sts.in>
2019-02-13 14:50:36 +0530

    complete-song-demo

--- src/components/songs/SongDetailsComponent.js
+++ src/components/songs/SongDetailsComponent.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetails = (props) => {
-    return (
-        <div>SongDetails</div>
-    );
+const SongDetails = ({ song }) => {
+    if (song != null) {
+        return (
+            <div>
+                <b>
+                    <p>Song Title : { song.title } </p>
+                    <p>Song Duration : { song.duration } </p>
+                </b>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h5>No Song Selected!</h5>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
